442d1b8defc7fc734d91f22651f18a1b9008a4a1
Alain <hello@alainbarthelemy.com>
2016-08-03 15:30:35 +0200

    bullets center

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-//define('DB_NAME', 'flaremetadb');
-define('DB_NAME', 'flair-meta');
+define('DB_NAME', 'flaremetadb');
+//define('DB_NAME', 'flair-meta');
 
 /** MySQL database username */
-//define('DB_USER', 'root');
-define('DB_USER', 'flair-meta');
+define('DB_USER', 'root');
+//define('DB_USER', 'flair-meta');
 
 /** MySQL database password */
-//define('DB_PASSWORD', 'root');
-define('DB_PASSWORD', 'Bpxb#g58&q3R');
+define('DB_PASSWORD', 'root');
+//define('DB_PASSWORD', 'Bpxb#g58&q3R');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
